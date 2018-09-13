8bf39d24f372614871da019172b43f07a99a1c78
yogesh9d <34537323+yogesh9d@users.noreply.github.com>
2018-09-13 23:38:29 +0530

    Add files via upload

--- GameFrame.java
+++ GameFrame.java
@@ -2,19 +2,23 @@ import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.*;
 
-/*<applet code="GameFrame.class" width="710" height="510"> </applet>  */
+/*<applet code="GameFrame.class" width="710" height="510"> </applet>*/
+
 
 public class GameFrame extends Applet implements MouseListener, MouseMotionListener, KeyListener, Runnable {
+
+    boolean chance = true;
+    boolean isFirstFrame = true;
+    boolean gameOver = false;
+
     int frameXLimit = 700;
     int frameYLimit = 500;
 
-    boolean isFirstFrame = true;
-    Font font;
-
-    /**************Safe Modify****************/
-    float fps = (float) 6;  // DEFAULT GAME SPEED
+    /**************SAFE MODIFY****************/
+    float fps = (float) 10;  // DEFAULT GAME SPEED
     int initialLength = 3;  // DEFAULT INITIAL SNAKE LENGTH
 
+//    DEFAULT COLORS
     Color headColor = Color.blue;
     Color bodyColor = Color.black;
     Color scoreColor = Color.white;
@@ -23,30 +27,37 @@ public class GameFrame extends Applet implements MouseListener, MouseMotionListe
     Color textColor = Color.green;
     Color foodColor = Color.green;
 
+//    DEFAULT CONTROLS
+    char up = 'w';
+    char down = 's';
+    char left = 'a';
+    char right = 'd';
+
+//    DEFAULT DIRECTION
+    char initialDirection = right;
+
     /*****************************************/
     Snake snake;
     Food food;
     Thread theFrameThread;
-    Thread snakeThread;
-
 
     int fpsSleepTime = Math.round(1000 / fps);
 
     @Override
     public void run() {
-//            System.out.println("frame is running");
+//            System.out.println("frame is running"); //DEBUG
 
-        snakeThread.start();
-        while (!snake.gameOver) {
+        while (!gameOver) {
+//            System.out.println("game running"); //DEBUG
+
 //            DEFAULT MOVE
-            if (!snake.moveMadeByPlayer) {
+            if (!snake.moveMadeByPlayer)
                 snake.moveSnake();
-                snake.frameChance = true;
-            }
 
 //            PAINTING FRAME IF SNAKE CHANCE
-            if (snake.frameChance) {
+            if (chance) {
                 repaint();
+
                 try {
                     Thread.sleep(fpsSleepTime);
                 } catch (InterruptedException e) {
@@ -56,24 +67,27 @@ public class GameFrame extends Applet implements MouseListener, MouseMotionListe
         }
     }
 
+    public void update(Graphics g){
+        paint(g);
+    }
+
     public void paint(Graphics g) {
 
+//        System.out.println("painting frame"); //DEBUG
         Graphics2D G = (Graphics2D) g;
 
-//        BORDER PRINTING
-        g.setColor(borderColor);
-        g.drawRect(1, 1, frameXLimit, frameYLimit);
-
-//        FIRST SPECIAL FRAMES
+/********************************COUNTDOWN***************************************/
         if (isFirstFrame) {
+//        BORDER PRINTING
+            g.setColor(borderColor);
+            g.drawRect(1, 1, frameXLimit-1, frameYLimit-1);
 
             g.setColor(textColor);
-            font = new Font(Font.MONOSPACED, Font.BOLD, 40);
-            g.setFont(font);
+            g.setFont(new Font(Font.MONOSPACED, Font.BOLD, 40));
 
 //            GAME BEGINS COUNTDOWN
             for (int i = 0; i < 7; i++) {
-                g.clearRect(10, 10, frameXLimit - 10, frameYLimit - 10);
+                g.clearRect(2, 2, frameXLimit-2, frameYLimit-2);
 
                 if (i == 0)
                     g.drawString("GAME BEGINS IN", Math.round(frameXLimit / 2) - 170, Math.round(frameYLimit / 2));
@@ -86,53 +100,61 @@ public class GameFrame extends Applet implements MouseListener, MouseMotionListe
                     e.printStackTrace();
                 }
             }
+            g.clearRect(10, 10, frameXLimit - 10, frameYLimit - 10);
             isFirstFrame = false;
         }
+/***************************************************************************************/
 
 
-//        CHECKING IF FOOD IS EATEN
-        if (!food.isFoodValid()) {
-            snake.addTail();
-            food.createFood();
-        }
+/***********************************GAMEPLAY********************************************/
+        else{
+            g.clearRect(10, 10, 300, 30);
 
-        if (snake.isAliveee()) {
 //            FOOD PRINTING
             g.setColor(foodColor);
-            G.fillOval(food.foodX, food.foodY, 10, 10);
+            G.fillRect(food.foodX, food.foodY, 10, 10);
 
 //            BODY PRINTING
-            g.setColor(Color.black);
-            for (int i = 0; i < snake.x.size(); i++) {
+            for (int i = snake.x.size()-1 ; i >= 0; i--) {
                 if (i == 0)
                     g.setColor(headColor);
                 else
                     g.setColor(bodyColor);
-                G.fillOval(snake.x.get(i), snake.y.get(i), 10, 10);
+                G.fillRect(snake.x.get(i), snake.y.get(i), 10, 10);
             }
+            g.clearRect(snake.deadX, snake.deadY, 10, 10);
 
 //            SCORE PRINTING
             g.setColor(scoreColor);
-            font = new Font(Font.MONOSPACED, Font.PLAIN, 20);
+            g.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 20));
             g.drawString(Integer.toString((snake.x.size() - initialLength) * 7), 30, 30);
+
+//            GAME OVER PRINTING
+            if(!snake.isAliveee()) {
+                gameOver = true;
+
+                g.setColor(textColor);
+                g.setFont(new Font(Font.MONOSPACED, Font.BOLD, 40));
+
+                g.drawString("GAME OVER", Math.round(frameXLimit / 2) - 120, Math.round(frameYLimit / 2));
+
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+//            PAINTING BORDER
+            g.drawRect(1,1, frameXLimit+1, frameYLimit+1);
+
+//            RESETS AFTER EACH GAME PLAY FRAME
+            chance = false;
+            snake.chance = true;
+            snake.moveMadeByPlayer = false;
         }
 
-//        GAME OVER PRINTING
-        else  {
-            snake.gameOver = true;
-            font = new Font(Font.MONOSPACED, Font.BOLD, 40);
-            g.setFont(font);
-            g.setColor(Color.green);
-            g.drawString("GAME OVER", Math.round(frameXLimit / 2) - 120, Math.round(frameYLimit / 2));
-        }
-
-//        RESETS AFTER EACH FRAME
-        snake.frameChance = false;
-        snake.snakeChance = true;
-        snake.moveMadeByPlayer = false;
-        snake.keyPressedInSnake = false;
-
-//        System.out.println("snake's chance"); //DEBUG
     }
 
     @Override
@@ -141,16 +163,11 @@ public class GameFrame extends Applet implements MouseListener, MouseMotionListe
         addMouseListener(this);
         addMouseMotionListener(this);
         addKeyListener(this);
-        addKeyListener(snake);
 
         this.setSize(new Dimension(frameXLimit, frameYLimit));
 
-        snake = new Snake(initialLength, frameXLimit, frameYLimit);
-        snakeThread = new Thread(snake);
-
+        snake = new Snake(this);
         theFrameThread = new Thread(this);
-        snake.theFrameThread = theFrameThread;
-
         food = new Food(snake, this);
 
         setBackground(backGroundColor);
@@ -163,22 +180,16 @@ public class GameFrame extends Applet implements MouseListener, MouseMotionListe
 
     @Override
     public void keyTyped(KeyEvent e) {
-//        System.out.println("snake chance now inn frame"); //DEBUG
-        if (snake.snakeChance && !snake.keyPressedInSnake && !snake.moveMadeByPlayer) {
-            snake.keyPressedInSnake = true;
-//            System.out.println("key pressed in frame" + e.getKeyChar()); //DEBUG
+        if (snake.chance && !snake.moveMadeByPlayer) {
+//            System.out.println("key pressed in frame - " + e.getKeyChar()); //DEBUG
+
             snake.setDirection(e.getKeyChar());
-            if (snake.snakeChance && snake.keyPressedInSnake) {
-//                System.out.println("moving by player's will - " + snake.direction); //DEBUG
-                snake.moveSnake();
-                snake.snakeChance = false;
-                snake.frameChance = true;
-                snake.keyPressedInSnake = true;
-                snake.moveMadeByPlayer = true;
-            }
+
+            snake.moveSnake();
         }
     }
 
+/**********************************UNNECESSARY******************************************/
     @Override
     public void stop() {
 
--- Snake.java
+++ Snake.java
@@ -1,82 +1,43 @@
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+public class Snake extends Body{
+    GameFrame gameFrame;
 
-public class Snake extends Body implements Runnable, KeyListener {
-    volatile Character direction = new Character('d');
+    boolean chance = false;
+    boolean moveMadeByPlayer = false;
+    char direction;
 
-    Thread theFrameThread;
-
-    volatile int length = 5; // DEFAULT LENGTH
-    volatile int frameXLimit = 700; // DEFAULT FRAME SIZE
-    volatile int frameYLimit = 700; // DEFAULT FRAME SIZE
-
-    volatile boolean keyPressedInSnake = false;
-    volatile boolean frameChance = true;
-    volatile boolean snakeChance = false;
-    volatile boolean moveMadeByPlayer = true;
-
-    public Snake(int length, int frameXLimit, int frameYLimit) {
-
-        super(length, frameXLimit, frameYLimit);
-        this.length = length;
-        this.frameXLimit = frameXLimit;
-        this.frameYLimit = frameYLimit;
-
+    public Snake(GameFrame gameFrame) {
+        super(gameFrame);
+        this.gameFrame = gameFrame;
+        direction = gameFrame.initialDirection;
     }
 
-    public void setDirection(Character p) {
-//        System.out.println("setting direction");
-        if (direction == 'w' && p != 's')
-            direction = p;
-        else if (direction == 's' && p != 'w')
-            direction = p;
-        else if (direction == 'a' && p != 'd')
-            direction = p;
-        else if (direction == 'd' && p != 'a')
-            direction = p;
+    public void setDirection(Character newDirection) {
+//        System.out.println("setting direction"); //DEBUG
+
+        if(newDirection == gameFrame.up && direction != gameFrame.down)
+            direction = newDirection;
+        else if(newDirection == gameFrame.down && direction != gameFrame.up)
+            direction = newDirection;
+        else if(newDirection == gameFrame.right && direction != gameFrame.left)
+            direction = newDirection;
+        else if(newDirection == gameFrame.left && direction != gameFrame.right)
+            direction = newDirection;
+
     }
 
     public void moveSnake() {
+        moveMadeByPlayer = true;
 //        System.out.println("moving snake");
         addHead(direction);
         removeTail();
-    }
 
-    @Override
-    public void run() {
-//        System.out.println("snake is running"); //DEBUG
-        while (!gameOver) {
-            if (theFrameThread.getState() != Thread.State.TIMED_WAITING)
-                Thread.yield();
+//        CHECKING IF FOOD IS EATEN
+        if (!gameFrame.food.isFoodValid()) {
+            addTail();
+            gameFrame.food.createFood();
         }
-    }
 
-    @Override
-    public void keyTyped(KeyEvent e) {
-//        System.out.println("snake chance now inn snake");
-        if (snakeChance && !keyPressedInSnake && !moveMadeByPlayer) {
-            keyPressedInSnake = true;
-//            System.out.println("key pressed in snake" + e.getKeyChar());
-            if (e.getKeyChar() != direction)
-                setDirection(e.getKeyChar());
-            if (snakeChance && keyPressedInSnake) {
-//                System.out.println("moving by player's will - " + direction);
-                moveSnake();
-                snakeChance = false;
-                frameChance = true;
-                keyPressedInSnake = true;
-                moveMadeByPlayer = true;
-            }
-        }
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-
+        chance = false;
+        gameFrame.chance = true;
     }
 }
